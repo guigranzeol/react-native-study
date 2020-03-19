de9fb63f2b20ff07f678955e7f087b4a763c1cb7
Guilherme Granzeol <guilhermegranzeol@MacBook-Pro-de-Guilherme.local>
2020-03-19 00:39:27 -0300

    mem update

---
+++ src/pages/main.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'; 
+import { StyleSheet, Text, View} from 'react-native'; 
+
+export default class Main extends Component{  
+    render() {  
+      return (  
+        <View style={styles.container}>  
+          <Text style={styles.welcome}>This is Main</Text>  
+          </View>  
+      );  
+    }  
+  } 
